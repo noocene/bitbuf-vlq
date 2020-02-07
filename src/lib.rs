5b26120526104e3ea025895dc2c67dad343d2c9d
Izzy Swart <zenerboson@gmail.com>
2020-02-07 11:55:10 -0800

    boundary condition tests, fix for large values

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@
 //! that can contain any u64, occupying between 1 and 9 bytes
 //! and wasting one bit per byte in serialized form.
 //! ```
+//! use bitbuf::{BitBuf, BitBufMut};
+//! use bitbuf_vlq::Vlq;
 //! let mut data = [0u8; 8];
 //!
 //! // Very large number (requires 48 bits)
@@ -51,47 +53,25 @@
 use bitbuf::{BitBuf, BitBufMut, CopyError};
 use core::ops::Deref;
 
-macro_rules! offset {
-    (1) => {
-        0
-    };
-    (2) => {
-        2u16.pow(7)
-    };
-    (3) => {
-        offset!(2) as u32 + 2u32.pow(14)
-    };
-    (4) => {
-        offset!(3) as u32 + 2u32.pow(21)
-    };
-    (5) => {
-        offset!(4) as u64 + 2u64.pow(28)
-    };
-    (6) => {
-        offset!(5) + 2u64.pow(35)
-    };
-    (7) => {
-        offset!(6) + 2u64.pow(42)
-    };
-    (8) => {
-        offset!(7) + 2u64.pow(49)
-    };
-    (9) => {
-        offset!(8) + 2u64.pow(56)
-    };
-}
-
 fn encode_len(n: u64) -> u8 {
-    match n {
-        n if n < offset!(2) as u64 => 0,
-        n if n < offset!(3) as u64 => 1,
-        n if n < offset!(4) as u64 => 2,
-        n if n < offset!(5) => 3,
-        n if n < offset!(6) => 4,
-        n if n < offset!(7) => 5,
-        n if n < offset!(8) => 6,
-        n if n < offset!(9) => 7,
-        _ => 8,
+    if n < 2u64.pow(7) {
+        0
+    } else if n < 2u64.pow(14) {
+        1
+    } else if n < 2u64.pow(20) {
+        2
+    } else if n < 2u64.pow(28) {
+        3
+    } else if n < 2u64.pow(35) {
+        4
+    } else if n < 2u64.pow(42) {
+        5
+    } else if n < 2u64.pow(49) {
+        6
+    } else if n < 2u64.pow(56) {
+        7
+    } else {
+        8
     }
 }
 
@@ -120,7 +100,9 @@ impl<T: Into<u64>> From<T> for Vlq {
         for _ in 0..len {
             buf.push(false).unwrap();
         }
-        buf.push(true).unwrap();
+        if len != 8 {
+            buf.push(true).unwrap();
+        }
         let len = match len {
             0 => 7,
             1 => 14,
@@ -160,10 +142,11 @@ impl Vlq {
         while let Some(item) = buf.pop() {
             if item {
                 break;
-            }
-            len += 1;
-            if len == 9 {
-                break;
+            } else {
+                len += 1;
+                if len == 8 {
+                    break;
+                }
             }
         }
         len = match len {
@@ -176,7 +159,7 @@ impl Vlq {
             6 => 49,
             7 => 56,
             8 => 64,
-            _ => return Err(Error::TooLong),
+            _ => panic!("invalid length in Vlq read"),
         };
         let mut data = [0u8; 8];
         buf.copy_to_slice(&mut data, len)?;
@@ -184,5 +167,124 @@ impl Vlq {
             *byte = byte.reverse_bits();
         }
         Ok(u64::from_le_bytes(data))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn read_write(value: u64, bytes: usize) {
+        // Create backing storage
+        let mut data = vec![0u8; bytes];
+
+        // Create a buffer handle for writing
+        let mut buf = BitBufMut::new(&mut data);
+
+        // Create a vlq
+        let vlq = Vlq::from(value);
+
+        // Write vlq to buffer
+        buf.put_aligned(&*vlq)
+            .expect("writing vlq to buffer failed");
+
+        // Ensure the correct byte length was written
+        assert_eq!(buf.len(), bytes * 8);
+
+        // Read vlq to ensure value is preserved
+        assert_eq!(
+            Vlq::read(&mut BitBuf::new(&data)).expect("reading vlq failed"),
+            value
+        );
+    }
+
+    #[test]
+    fn u7_lower_bound() {
+        read_write(0, 1);
+    }
+
+    #[test]
+    fn u7_upper_bound() {
+        read_write(2u64.pow(7) - 1, 1);
+    }
+
+    #[test]
+    fn u14_lower_bound() {
+        read_write(2u64.pow(7), 2);
+    }
+
+    #[test]
+    fn u14_upper_bound() {
+        read_write(2u64.pow(14) - 1, 2);
+    }
+
+    #[test]
+    fn u20_lower_bound() {
+        read_write(2u64.pow(14), 3);
+    }
+
+    #[test]
+    fn u20_upper_bound() {
+        read_write(2u64.pow(20) - 1, 3);
+    }
+
+    #[test]
+    fn u28_lower_bound() {
+        read_write(2u64.pow(20), 4);
+    }
+
+    #[test]
+    fn u28_upper_bound() {
+        read_write(2u64.pow(28) - 1, 4);
+    }
+
+    #[test]
+    fn u35_lower_bound() {
+        read_write(2u64.pow(28), 5);
+    }
+
+    #[test]
+    fn u35_upper_bound() {
+        read_write(2u64.pow(35) - 1, 5);
+    }
+
+    #[test]
+    fn u42_lower_bound() {
+        read_write(2u64.pow(35), 6);
+    }
+
+    #[test]
+    fn u42_upper_bound() {
+        read_write(2u64.pow(42) - 1, 6);
+    }
+
+    #[test]
+    fn u49_lower_bound() {
+        read_write(2u64.pow(42), 7);
+    }
+
+    #[test]
+    fn u49_upper_bound() {
+        read_write(2u64.pow(49) - 1, 7);
+    }
+
+    #[test]
+    fn u56_lower_bound() {
+        read_write(2u64.pow(49), 8);
+    }
+
+    #[test]
+    fn u56_upper_bound() {
+        read_write(2u64.pow(56) - 1, 8);
+    }
+
+    #[test]
+    fn u64_lower_bound() {
+        read_write(2u64.pow(56), 9);
+    }
+
+    #[test]
+    fn u64_upper_bound() {
+        read_write(core::u64::MAX, 9);
     }
 }
